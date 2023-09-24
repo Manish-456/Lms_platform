d4760a748a42fd4ed6e1794ad86ca3739eb9c872
Manish <tamangmanish3456@gmail.com>
2023-09-24 14:49:57 +0545

    part-10 : added /api/create with POST METHOD

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -42,6 +42,7 @@ export default function CreatePage() {
     try {
       const response = await axios.post("/api/courses", values);
       router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created");
     } catch {
       toast.error("Something went wrong.")
     }
---
+++ app/api/courses/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req : Request){
+    try {
+        const {userId} = auth();
+        const {title} = await req.json();
+
+        if(!userId) return new NextResponse("Unauthorized", {status : 401});
+        if(!title) return new NextResponse("Title is required", {status : 400});
+
+        const course = await db.course.create({
+            data : {
+                title,
+                userId
+            }
+        });
+
+        return NextResponse.json(course);
+
+
+    } catch (error : any) {
+        console.error(`[COURSES.POST]`, error.message);
+        return new NextResponse("Internal Error", {
+            status : 500
+        })
+    }
+}

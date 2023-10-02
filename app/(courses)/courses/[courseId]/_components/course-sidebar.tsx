0d9105bef32fd256ab7edcb20d30a73ed79ea88b
Manish <tamangmanish3456@gmail.com>
2023-10-02 15:41:03 +0545

    added course progress bar in sidebar

--- app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
@@ -4,6 +4,7 @@ import { Course, Chapter,UserProgress } from '@prisma/client'
 import { redirect } from 'next/navigation'
 import { db } from '@/lib/db'
 import CourseSidebarItem from './course-sidebar-item'
+import CourseProgress from '@/components/course-progress'
 
 interface CourseSidebarProps {
     course : Course & {
@@ -27,9 +28,17 @@ export default async function CourseSidebar({course, progressCount} : CourseSide
 
   return (
     <div className='h-full border-r flex flex-col overflow-y-auto shadow-sm'>
-      <div className='p-8 flex flex-col border-b'>
+      <div className='p-8 flex  flex-col border-b'>
         <h1 className='font-semibold'>{course.title}</h1>
-        {/* Check purchase and add progress */}
+
+        {
+          purchase && <div className="mt-10">
+            <CourseProgress
+            variant={"success"}
+            value={progressCount!}
+            />
+          </div>
+        }
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
